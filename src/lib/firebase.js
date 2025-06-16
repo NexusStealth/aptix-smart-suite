62f9d2808b1afbe11659de8ab187a52f4ed1fe98
ytallo <nexusstealth312@gmail.com>
2025-06-16 02:04:44 -0300

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+// src/lib/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXOkx58VX_reKBdUAVQ8xTtJoNr0TPVsM",
+  authDomain: "aptix-80019.firebaseapp.com",
+  projectId: "aptix-80019",
+  storageBucket: "aptix-80019.appspot.com",
+  messagingSenderId: "1013152306674",
+  appId: "1:1013152306674:web:34a6506d662baf68ec476b",
+  measurementId: "G-SXWSL2SBTT"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
